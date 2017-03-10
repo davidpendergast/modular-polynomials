28083b94ec00db82295ff2c6527409cab74289dc
dpendergast <dlp75@case.edu>
2017-03-10 18:03:01 -0500

    filled in the field opperations of QuotientGroup

--- src/polynomials/QuotientGroup.java
+++ src/polynomials/QuotientGroup.java
@@ -11,6 +11,36 @@ public class QuotientGroup {
         this.poly = base;
     }
     
+    public Polynomial add(Polynomial p1, Polynomial p2) {
+        modCheck(p1,p2);
+        return reduce(PolyMath.add(p1, p2));
+    }
+    
+    public Polynomial pow(Polynomial p, int n) {
+        modCheck(p);
+        return reduce(PolyMath.pow(p, n));
+    }
+    
+    public Polynomial multiply(Polynomial p1, Polynomial p2) {
+        modCheck(p1,p2);
+        return reduce(PolyMath.multiply(p1, p2));
+    }
+    
+    private void modCheck(Polynomial...polynomials) {
+        int m = poly.getModulus();
+        for (Polynomial p : polynomials) {
+            if (p.getModulus() != m) {
+                throw new IllegalArgumentException("Polynomial must have same"
+                        + " modulus as the QuotientRing.");
+            }
+        }
+    }
+    
+    public Polynomial reduce(Polynomial p) {
+        modCheck(p);
+        return PolyMath.divide(p, poly)[1];
+    }
+    
     public int getModulus() {
         return poly.getModulus();
     }
